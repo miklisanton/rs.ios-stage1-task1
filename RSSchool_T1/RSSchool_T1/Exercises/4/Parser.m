13a03f7098b28cb9ee4390daff49b921bcb0ccde
anton <antonmiklis@icloud.com>
2021-04-24 18:18:30 +0300

    feat: add task3

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,35 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray *output = [NSMutableArray array];
+    
+    for (int i = 0; i < [string length]; i++) {
+        if ([string characterAtIndex:i] == '<') {
+            int startIndex = i;
+            while ([string characterAtIndex:i] != '>') {
+                i++;
+            }
+            int endIndex = i;
+            [output addObject:[string substringWithRange:NSMakeRange(startIndex + 1, endIndex - startIndex - 1)]];
+        }
+        if ([string characterAtIndex:i] == '(') {
+            int startIndex = i;
+            while ([string characterAtIndex:i] != ')') {
+                i++;
+            }
+            int endIndex = i;
+            [output addObject:[string substringWithRange:NSMakeRange(startIndex + 1, endIndex - startIndex - 1)]];
+        }
+        if ([string characterAtIndex:i] == '[') {
+            int startIndex = i;
+            while ([string characterAtIndex:i] != ']') {
+                i++;
+            }
+            int endIndex = i;
+            [output addObject:[string substringWithRange:NSMakeRange(startIndex + 1, endIndex - startIndex - 1)]];
+        }
+    }
+    return output;
 }
 
 @end
