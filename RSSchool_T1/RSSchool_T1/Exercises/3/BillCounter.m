28247bdb1e5bbcc15d363c4422d6f38bc51d0e1a
anton <antonmiklis@icloud.com>
2021-04-24 16:31:44 +0300

    feat: add task3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,14 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    int briansItem = [bill[index] intValue];
+    int totalPrice = [[bill valueForKeyPath:@"@sum.self"] intValue];;
+    if ((totalPrice - briansItem) / 2 == [sum intValue]  ) {
+        return @"Bon Appetit";
+    }
+    int overcharge = [sum intValue] - (totalPrice - briansItem) / 2;
+    NSString *output = [NSString stringWithFormat:@"%d", overcharge];
+    return output;
 }
 
 @end
