47cc0bcc158ffde82fee8ffdeb7336bd860b236e
anton <antonmiklis@icloud.com>
2021-04-24 14:40:48 +0300

    feat: add task1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,25 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if([sadArray count] == 0){
+        return sadArray;
+    }
+    
+    NSMutableArray *sadMutableArray= [sadArray mutableCopy];
+    
+    for (int i = 1; i < [sadMutableArray count] - 1; i++) {
+        if([sadMutableArray[i] intValue] > [sadMutableArray[i + 1] intValue] + [sadMutableArray[i - 1] intValue]){
+            [sadMutableArray removeObjectAtIndex:i];
+        }
+    }
+    
+    NSArray *tempArray = [sadMutableArray copy];
+    
+    if ([sadArray count] != [sadMutableArray count]) {
+        tempArray = [self convertToHappy:tempArray];
+    }
+    
+    return tempArray;
 }
 
 @end
