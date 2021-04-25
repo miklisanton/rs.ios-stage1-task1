d62533b688009c55099e1b88cc068a2ef2854ff8
anton <antonmiklis@icloud.com>
2021-04-25 19:28:21 +0300

    fix: task4 logic

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -5,23 +5,35 @@
 - (NSArray <NSString*>*)parseString:(NSString*)string{
     NSMutableArray *output = [NSMutableArray array];
     NSDictionary *brackets = @{
-        @"<": @">",
-        @"[": @"]",
         @"(": @")",
+        @"[": @"]",
+        @"<": @">",
     };
-    for(NSString *key in brackets){
-        int start = -1;
-        int end = -1;
-        for (int i = 0; i < [string length]; i++) {
-            if ([[string substringWithRange:NSMakeRange(i, 1)] isEqualToString: key]) {
-                start = i;
-            }
-            if ([[string substringWithRange:NSMakeRange(i, 1)] isEqualToString: brackets[key]]) {
-                end = i;
+    for (int i = 0; i < [string length]; i++) {
+        unichar element = [string characterAtIndex:i];
+        for(NSString *key in brackets){
+            //NSMutableString *word = [[NSMutableString alloc]init];
+            unichar key_char = [key characterAtIndex:0];
+            int j = i;
+            if(key_char == element){
+                int start = i;
+                int omit = 0;
+                while([string characterAtIndex:j] != [brackets[key] characterAtIndex:0]){
+                    j++;
+                    if (key_char == [string characterAtIndex:j]) {
+                        omit++;
+                    }
+                    if([brackets[key] characterAtIndex:0] == [string characterAtIndex:j] && omit != 0){
+                        omit--;
+                        j++;
+                    }
+                }
+                int end = j;
                 [output addObject:[string substringWithRange:NSMakeRange(start + 1, end - start - 1)]];
             }
         }
     }
+    NSLog(@"%@", output);
     return output;
 }
 
