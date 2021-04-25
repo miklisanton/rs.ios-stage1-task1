3f0b8835917fccd16e31ce149a75ea7abade0a02
anton <antonmiklis@icloud.com>
2021-04-25 23:05:31 +0300

    feat: add task5

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,33 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    NSString *q = @"";
+    unsigned long length = [s length];
+    int flag = 0;
+    for (int i = 0; i < length / 2; i++) {
+        if ([s characterAtIndex:i] != [s characterAtIndex:[s length] - 1]) {
+            q = [s stringByReplacingCharactersInRange:NSMakeRange(i, 1)
+                                           withString:[s substringWithRange:NSMakeRange(length-i-1,1)]];
+            s = [s stringByReplacingCharactersInRange:NSMakeRange(length - i - 1, 1)
+                                           withString:[s substringWithRange:NSMakeRange(i,1)]];
+            s = [NSString stringWithFormat:@"%li", MAX(s.integerValue, q.integerValue)];
+            flag++;
+        }
+        if (flag > [k intValue]) {
+            return @"-1";
+        }
+    }
+    for (int i = 0; i < length / 2; i++) {
+        if([k intValue] - flag == 0 || [k intValue] - flag == -1){
+            break;
+        }
+        if([s characterAtIndex:i] != '9'){
+            s = [s stringByReplacingCharactersInRange:NSMakeRange(length - i - 1, 1)withString: @"9"];
+            s = [s stringByReplacingCharactersInRange:NSMakeRange(i, 1)withString: @"9"];
+            flag += 2;
+        }
+    }
+    return s;
 }
 
 @end
